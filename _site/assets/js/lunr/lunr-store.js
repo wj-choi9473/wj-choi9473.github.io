a7836ea74714c4de2bf69578cc6341b923a3bcef
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2020-09-17 22:45:15 +0900

    upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,10 +4,4 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/cheatsheets/markdown/",
         "teaser": null
-      },{
-        "title": "Example of Post",
-        "excerpt":"This formula $f(x) = x^2$ is an example.  This formula $f(x) = x^2$ is an example.   \\[1+1\\] ","categories": ["Example"],
-        "tags": [],
-        "url": "http://localhost:4000/example/postexample/",
-        "teaser": null
       }]
